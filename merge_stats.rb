dd581a182b64dfe8f0256809bd473c8578914153
Oyjord <oyjord@gmail.com>
2025-10-29 17:34:21 -0700

    Create merge_stats.rb

---
+++ merge_stats.rb
@@ -0,0 +1,13 @@
+
+require "json"
+
+skaters = JSON.parse(File.read("output/swamp_skaters.json"))
+goalies = JSON.parse(File.read("output/swamp_goalies.json"))
+
+merged = {
+  skaters: skaters,
+  goalies: goalies
+}
+
+File.write("output/reign_stats.json", JSON.pretty_generate(merged))
+puts "✅ Merged stats written to output/swamp_stats.json"
